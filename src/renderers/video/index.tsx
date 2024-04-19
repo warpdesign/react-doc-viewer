b14c8b15f88a4b53b11e96e95c8b58683bdbd237
Nicolas Ramz <nicolas.ramz@adevinta.com>
2024-04-19 15:13:07 +0200

    VideoRenderer: don't preload the entire video
    This was done by adding a fake fileLoader for the video Renderer that
    completes immediately.

--- src/renderers/video/index.tsx
+++ src/renderers/video/index.tsx
@@ -1,21 +1,28 @@
-import React from "react";
-import styled from "styled-components";
-import { DocRenderer } from "../..";
+import React from "react"
+import styled from "styled-components"
+import { DocRenderer } from "../.."
 
 const VideoRenderer: DocRenderer = ({ mainState: { currentDocument } }) => {
-  if (!currentDocument) return null;
+  if (!currentDocument) return null
 
   return (
     <Container id="video-renderer">
-      <Video controls src={currentDocument.uri} />
+      <Video controls src={currentDocument.uri} autoPlay />
     </Container>
   );
 };
 
-export default VideoRenderer;
+export default VideoRenderer
 
-VideoRenderer.fileTypes = ["video/mp4", "video/quicktime", "video/x-msvideo"];
-VideoRenderer.weight = 0;
+VideoRenderer.fileTypes = ["video/mp4", "video/quicktime", "video/x-msvideo"]
+VideoRenderer.weight = 0
+// Define a fake file loader for videos: the video tag will automatically stream
+// the video so we don't need to load the whole (!) file before.
+VideoRenderer.fileLoader = ({ fileLoaderComplete }) => {
+  fileLoaderComplete({
+    result: null
+  } as FileReader)
+}
 
 const Container = styled.div`
   width: 100%;
