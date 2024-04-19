7e3e58eff4c99388c63adaff9b96051112ebaa87
Nicolas Ramz <nicolas.ramz@adevinta.com>
2024-04-19 15:13:07 +0200

    VideoRenderer: don't preload the entire video
    This was done by adding a fake fileLoader for the video Renderer that
    completes immediately.

--- src/models.ts
+++ src/models.ts
@@ -3,79 +3,80 @@ import { IMainState } from "./store/mainStateReducer";
 import { FileLoaderFunction } from "./utils/fileLoaders";
 
 export interface IConfig {
-  header?: IHeaderConfig;
-  loadingRenderer?: ILoadingRendererConfig;
-  noRenderer?: INoRendererConfig;
-  csvDelimiter?: string;
-  pdfZoom?: IPdfZoomConfig;
-  pdfVerticalScrollByDefault?: boolean;
+  header?: IHeaderConfig
+  loadingRenderer?: ILoadingRendererConfig
+  noRenderer?: INoRendererConfig
+  csvDelimiter?: string
+  pdfZoom?: IPdfZoomConfig
+  pdfVerticalScrollByDefault?: boolean
 }
 
 export interface ILoadingRendererConfig {
   overrideComponent?: ComponentType<{
-    document: IDocument | undefined;
-    fileName: string;
+    document: IDocument | undefined
+    fileName: string
   }>;
-  showLoadingTimeout?: false | number;
+  showLoadingTimeout?: false | number
 }
 
 export interface INoRendererConfig {
   overrideComponent?: ComponentType<{
-    document: IDocument | undefined;
-    fileName: string;
+    document: IDocument | undefined
+    fileName: string
   }>;
 }
 
 export interface IHeaderConfig {
-  disableHeader?: boolean;
-  disableFileName?: boolean;
-  retainURLParams?: boolean;
-  overrideComponent?: IHeaderOverride;
+  disableHeader?: boolean
+  disableFileName?: boolean
+  retainURLParams?: boolean
+  overrideComponent?: IHeaderOverride
 }
 
 export interface IPdfZoomConfig {
-  defaultZoom: number;
-  zoomJump: number;
+  defaultZoom: number
+  zoomJump: number
 }
 
 export type IHeaderOverride = (
   state: IMainState,
   previousDocument: () => void,
   nextDocument: () => void,
-) => ReactElement<any, any> | null;
+) => ReactElement<any, any> | null
 
 export interface ITheme {
-  primary?: string;
-  secondary?: string;
-  tertiary?: string;
-  textPrimary?: string;
-  textSecondary?: string;
-  textTertiary?: string;
-  disableThemeScrollbar?: boolean;
+  primary?: string
+  secondary?: string
+  tertiary?: string
+  textPrimary?: string
+  textSecondary?: string
+  textTertiary?: string
+  disableThemeScrollbar?: boolean
 }
 
 export interface IStyledProps {
-  theme: ITheme;
+  theme: ITheme
 }
 
 export interface IDocument {
   uri: string;
-  fileType?: string;
-  fileData?: string | ArrayBuffer;
-  fileName?: string;
+  fileType?: string
+  fileData?: string | ArrayBuffer
+  fileName?: string
 }
 
 export interface DocRendererProps {
-  mainState: IMainState;
+  mainState: IMainState
 }
 
 export interface DocRenderer extends FC<PropsWithChildren<DocRendererProps>> {
-  fileTypes: string[];
-  weight: number;
-  fileLoader?: FileLoaderFunction | null | undefined;
+  fileTypes: string[]
+  weight: number
+  fileLoader?: FileLoaderFunction | null | undefined
+  streaming?: boolean
 }
 
 export interface DocViewerRef {
-  prev: () => void;
-  next: () => void;
+  prev: () => void
+  next: () => void
 }
